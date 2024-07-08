141e7988a555a85a8a26d130c6102e4467bd23b6
TuriColedani <turihendrix@gmail.com>
2024-07-08 19:15:46 -0300

    Add navbar components

--- src/app/views/layout.tsx
+++ src/app/views/layout.tsx
@@ -9,8 +9,17 @@ export default function RootLayout({
 }>) {
   return (
     <main className=" min-h-screen bg-background dark:bg-backgroundDark">
-            <header className=' z-50 fixed top-0 left-0 w-full'>
-                <nav className=" bg-backgroundDark dark:bg-background h-12 flex items-center justify-end px-5">
+            <header className=' bg-backgroundDark dark:bg-background z-50 fixed top-0 left-0 w-full h-12'>
+                <nav className=" w-[50vh] h-[80vh] border-2 flex flex-col absolute top-0 right-0 items-center justify-center px-5">
+                    <ul className=' flex flex-col'>
+                      <li>Mi Perfil</li>
+                      <li>Buscar</li>
+                      <li>Notificaciones</li>
+                      <li>Invitar Miembro</li>
+                      <li>Archivo</li>
+                      <li>Papelera</li>
+                      <li>Cerrar Sesión</li>
+                    </ul>
                     <ButtonTheme />
                 </nav>
             </header> 
--- src/components/SlideBar.tsx
+++ src/components/SlideBar.tsx
@@ -13,21 +13,21 @@ export default function SlideBar() {
 
 
     return (
-        <aside className={` p-5 py-16 w-2/3 ${!isSlideOpen ? "-translate-x-[93%]" : ""} fixed h-screen top-0 left-0 border-card border-r-2 flex items-center `}>
+        <aside className={`pl-5 py-16 w-2/3 ${!isSlideOpen ? "-translate-x-[93%]" : ""} fixed h-screen top-0 left-0 border-card border-r-2 flex items-center transition-all`}>
 
             <div className=' bg-card dark:bg-cardDark p-3 rounded-lg w-full h-full'>
-                <h2 className="text-2xl font-bold">Areas de trabajo</h2>
-                <ul className="list-none">
+                <h2 className=" w-full bg-primary rounded-md py-3 text-center text-2xl font-bold">Areas de trabajo</h2>
+                <ul className="list-none p-3">
                     <li>
-                        <Link href="#" className="block px-4 py-2 text-sm text-gray-700 hover:text-gray-900">MDC Trabajos</Link>
+                        <Link href="#" className=" w-full h-max block px-4 py-2 text-sm text-white hover:bg-background hover:text-black">MDC Trabajos</Link>
                     </li>
                     <li>
-                        <Link href="#" className="block px-4 py-2 text-sm text-gray-700 hover:text-gray-900">Compras</Link>
+                        <Link href="#" className=" w-full h-max block px-4 py-2 text-sm text-white hover:bg-background hover:text-black">Compras</Link>
                     </li>
                 </ul>  
             </div>
 
-            <button onClick={handleClick} className=' w-8 h-12 bg-cardDark dark:bg-card rounded-lg text-3xl font-semibold flex items-center justify-center translate-x-9'>
+            <button onClick={handleClick} className=' w-8 h-12 bg-cardDark dark:bg-card rounded-lg text-3xl font-semibold flex items-center justify-center translate-x-4'>
                 {isSlideOpen ? "<" : ">"}
             </button>
         </aside>
